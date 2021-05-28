46b7ac5eb073b1b8f2358f13a3e9f86dc85da6fe
Luke Jones <nnblc565@gmail.com>
2021-05-28 16:13:33 +0930

    Tested and fixed the Radar

--- Assets/Scripts/EnemyDetection/RadarSystem.cs
+++ Assets/Scripts/EnemyDetection/RadarSystem.cs
@@ -72,6 +72,8 @@ public class RadarSystem : MonoBehaviour
 
     [SerializeField]
     private List<Transform> _enemiesToTrack = new List<Transform>();
+
+    private List<Transform> _enemiesToRemove = new List<Transform>();
     /// <summary>
     /// Set the scale of the radar object
     /// </summary>
@@ -121,14 +123,9 @@ public class RadarSystem : MonoBehaviour
     private void UpdateBlips()
     {
         UpdateBlipSize();
-        //Loop over the enemy transforms and check if any have exited the radius
-        foreach (Transform t in _enemyBlips.Keys)
-            //Check if they have gone to far away from the radar
-            if (Vector3.Distance(t.position, m_worldReferencePoint.position) > m_radarRange)
-                //If so, remove them
-                LeaveRadar(t);
         //Loop over the enemies to track
         for (int i = 0; i < _enemiesToTrack.Count; i++)
+        {
             //If they are null or destroyed, remove them
             if (!_enemiesToTrack[i])
             {   //Remove them
@@ -136,12 +133,34 @@ public class RadarSystem : MonoBehaviour
                 //Reduce the indexer so we don't skip an enemy
                 i--;
             }
-        //Loop over the eneies we are tracking
-        foreach (Transform t in _enemiesToTrack)
             //Check if the enemy is close enough
-            if (Vector3.Distance(t.position, m_worldReferencePoint.position) <= m_radarRange)
+            else if (Vector3.Distance(_enemiesToTrack[i].position, m_worldReferencePoint.position) <= m_radarRange)
                 //If they are, add them to the radar
-                EnterRadar(t);
+                EnterRadar(_enemiesToTrack[i]);
+        }
+        //Loop over the enemy transforms and check if any have exited the radius
+        foreach (Transform t in _enemyBlips.Keys)
+        {
+            //Check if they have gone to far away from the radar
+            if (Vector3.Distance(t.position, m_worldReferencePoint.position) > m_radarRange)
+            {
+                //If so, remove them
+                LeaveRadar(t, false);
+                continue;
+            }
+            //Otherwise update the position of the blip relative to the radar
+            Transform blip = _enemyBlips[t];
+            //Set the local position of the blip
+            blip.localPosition = (t.position - m_worldReferencePoint.position) / (m_radarRange * 2);
+            blip.localPosition *= m_radarRadius;
+        }
+        //Because we can't remove the enemies from _enemyBlips in the foreach loop, we need to remove them now
+        for (int i = 0; i < _enemiesToRemove.Count; i++)
+        {   //Now we remove the enemy from the blips
+            _enemyBlips.Remove(_enemiesToRemove[0]);
+            //And remove the enemy from the enemies we need to remove
+            _enemiesToRemove.RemoveAt(0);
+        }
     }
     /// <summary>
     /// Updates the scale of each individual blip
@@ -155,7 +174,8 @@ public class RadarSystem : MonoBehaviour
     /// Removes the blip from the radar
     /// </summary>
     /// <param name="enemy">The enemy to create it for</param>
-    private void LeaveRadar(Transform enemy)
+    /// <param name="removeImmediately">Should the enemy be removed immediately. This should be used to avoid crashing a foreach iteration</param>
+    private void LeaveRadar(Transform enemy, bool removeImmediately = true)
     {   //If they didn't have a blip in the first place, don't remove theirs
         if (!_enemyBlips.ContainsKey(enemy))
             return;
@@ -163,8 +183,13 @@ public class RadarSystem : MonoBehaviour
         _inactiveBlips.Add(_enemyBlips[enemy]);
         //Dissable the blip
         _enemyBlips[enemy].gameObject.SetActive(false);
-        //Remove this enemy from the enemy blips
-        _enemyBlips.Remove(enemy);
+        //Check if the enemies should be removed immediately
+        if (removeImmediately)
+            //Remove this enemy from the enemy blips
+            _enemyBlips.Remove(enemy);
+        //Because we can't remove the enemies from _enemyBlips in the foreach loop, we need to remove them later
+        else
+            _enemiesToRemove.Add(enemy);
         //Keep tracking the enemy even after they leave
         _enemiesToTrack.Add(enemy);
     }
@@ -190,7 +215,7 @@ public class RadarSystem : MonoBehaviour
         }
         else
         {   //Otherwise we need to create a new blip
-            GameObject obj = Instantiate(m_blipPrefab, m_radarObject.transform);
+            GameObject obj = Instantiate(m_blipPrefab);
             //Scale the blip
             obj.transform.localScale = new Vector3(_radarBlipSize, _radarBlipSize, _radarBlipSize);
             //Store the blip
