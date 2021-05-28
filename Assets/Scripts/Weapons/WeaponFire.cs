231c623385e101e1c3fc5a01a93afcd98dc2c831
Zaydan Bickley <pokemaniac88@gmail.com>
2021-05-28 15:34:10 +0930

    working on early weapon systems

---
+++ Assets/Scripts/Weapons/WeaponFire.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFire : MonoBehaviour
+{
+    //public WeaponManager wm;
+    //private Weapon weapon;
+    /// <summary>
+    /// This block is temporary, for the very first weapon for testing
+    /// </summary>
+    public float damage;
+    public float projectileSpeed;
+    public Transform spawnPoint1, spawnPoint2;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OGInputGetter.Get(OGInputGetter.OculusInputs.BackTrigger))
+            Fire();
+    }
+
+    protected virtual void Fire()
+    {
+        GameObject b = Instantiate(bullet, spawnPoint1.position, gameObject.transform.rotation);
+        Projectile p = b.GetComponent<Projectile>();
+        p.damage = damage;
+        p.projectileSpeed = projectileSpeed;
+        if (spawnPoint2)
+        {
+            b = Instantiate(bullet, spawnPoint1.position, Quaternion.identity);
+            p = b.GetComponent<Projectile>();
+            p.damage = damage;
+            p.projectileSpeed = projectileSpeed;
+        }
+    }
+
+}
