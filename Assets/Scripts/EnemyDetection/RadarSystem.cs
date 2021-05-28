7ebfaf4fd978ed4374a99648350b6b8509b79687
Luke Jones <nnblc565@gmail.com>
2021-05-28 15:26:37 +0930

    Finished the Radar System.

---
+++ Assets/Scripts/EnemyDetection/RadarSystem.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Controls how the radar works and displays itself
+/// </summary>
+public class RadarSystem : MonoBehaviour
+{
+    /// <summary>
+    /// The size of the radar
+    /// </summary>
+    [Tooltip("The size of the radar")]
+    [SerializeField]
+    private float m_radarRadius = 1;
+    /// <summary>
+    /// The range of the radar
+    /// </summary>
+    public float m_radarRange = 10;
+    /// <summary>
+    /// The size of the radar
+    /// </summary>
+    public float RadarRadius
+    {
+        get => m_radarRadius;
+        set
+        {
+            m_radarRadius = value;
+            UpdateRadius();
+        }
+    }
+    /// <summary>
+    /// The sphere that represents the radar
+    /// </summary>
+    [Tooltip("The sphere that represents the radar")]
+    public GameObject m_radarObject = null;
+    /// <summary>
+    /// The equivalent point in the world that the center of the radar represents
+    /// </summary>
+    [Tooltip("The equivalent point in the world that the center of the radar represents")]
+    public Transform m_worldReferencePoint = null;
+    /// <summary>
+    /// The prefab that is spawned to represent the enemies on the radar
+    /// </summary>
+    [Tooltip("The prefab that is spawned to represent the enemies on the radar")]
+    public GameObject m_blipPrefab = null;
+    /// <summary>
+    /// The size of the enemyPrefabs
+    /// </summary>
+    [Tooltip("The size of the enemyPrefabs")]
+    [SerializeField]
+    private float _radarBlipSize = 0.1f;
+    /// <summary>
+    /// The size of the blips on the radar
+    /// </summary>
+    public float RadarBlipSize
+    {
+        get => m_radarRadius;
+        set
+        {
+            m_radarRadius = value;
+            UpdateBlipSize();
+        }
+    }
+    /// <summary>
+    /// Stores the enemy ships transform as a key to a blip gameObject
+    /// </summary>
+    private Dictionary<Transform, Transform> _enemyBlips = new Dictionary<Transform, Transform>();
+    /// <summary>
+    /// Stores all the blips that are not currently being used
+    /// </summary>
+    private List<Transform> _inactiveBlips = new List<Transform>();
+    /// <summary>
+    /// Set the scale of the radar object
+    /// </summary>
+    private void Start()
+    {   //Make sure the radar is there and scaled correctly
+        if (m_radarObject)
+            UpdateRadius();
+        else
+        {
+            Debug.LogError("The radar object has not been assigned");
+            Debug.Break();
+        }
+        //Make sure the blip prefab is set
+        if (!m_blipPrefab)
+        {
+            Debug.LogError("The blip prefab for the radar is not assigned!");
+            Debug.Break();
+        }
+        //Make sure the reference point is set
+        if (!m_worldReferencePoint)
+        {
+            Debug.LogError("The world reference point for the radar is not assigned");
+            Debug.Break();
+        }
+    }
+    /// <summary>
+    /// Updates the radar
+    /// </summary>
+    private void Update()
+    {
+#if UNITY_EDITOR
+        //If we are in the editor, keep updating the scale for debugging
+        UpdateRadius();
+#endif
+        UpdateBlips();
+    }
+    /// <summary>
+    /// Updates the radius of the sphere
+    /// </summary>
+    private void UpdateRadius()
+    {
+        m_radarObject.transform.localScale = new Vector3(m_radarRadius, m_radarRadius, m_radarRadius);
+    }
+    /// <summary>
+    /// Updates the position, rotation and scale of the blips
+    /// </summary>
+    private void UpdateBlips()
+    {
+        UpdateBlipSize();
+        //Loop over the enemy transforms and check if any have exited the radius
+        foreach (Transform t in _enemyBlips.Keys)
+            //Check if they have gone to far away from the radar
+            if (Vector3.Distance(t.position, m_worldReferencePoint.position) > m_radarRange)
+                //If so, remove them
+                LeaveRadar(t);
+    }
+    /// <summary>
+    /// Updates the scale of each individual blip
+    /// </summary>
+    private void UpdateBlipSize()
+    {   //Scale each of the blips to their correct size
+        foreach (Transform t in _enemyBlips.Values)
+            t.localScale = new Vector3(_radarBlipSize, _radarBlipSize, _radarBlipSize);
+    }
+    /// <summary>
+    /// Removes the blip from the radar
+    /// </summary>
+    /// <param name="enemy">The enemy to create it for</param>
+    private void LeaveRadar(Transform enemy)
+    {   //If they didn't have a blip in the first place, don't remove theirs
+        if (!_enemyBlips.ContainsKey(enemy))
+            return;
+        //Store the now inactive blip
+        _inactiveBlips.Add(_enemyBlips[enemy]);
+        //Dissable the blip
+        _enemyBlips[enemy].gameObject.SetActive(false);
+        //Remove this enemy from the enemy blips
+        _enemyBlips.Remove(enemy);
+    }
+    /// <summary>
+    /// Creates a blip on the radar
+    /// </summary>
+    /// <param name="enemy">The enemy to create it for</param>
+    public void EnterRadar(Transform enemy)
+    {   //Make sure they are not already in the radar
+        if (_enemyBlips.ContainsKey(enemy))
+            return;
+        //Check if we have a blip we can use
+        if (_inactiveBlips.Count > 0)
+        {
+            //Use the first blip in the list
+            _enemyBlips.Add(enemy, _inactiveBlips[0]);
+            //Enable the blip
+            _inactiveBlips[0].gameObject.SetActive(true);
+            //Make sure the scale is up to date
+            _inactiveBlips[0].localScale = new Vector3(_radarBlipSize, _radarBlipSize, _radarBlipSize);
+            //Remove the blip from blips
+            _inactiveBlips.RemoveAt(0);
+        }
+        else
+        {   //Otherwise we need to create a new blip
+            GameObject obj = Instantiate(m_blipPrefab, m_radarObject.transform);
+            //Scale the blip
+            obj.transform.localScale = new Vector3(_radarBlipSize, _radarBlipSize, _radarBlipSize);
+            //Store the blip
+            _enemyBlips.Add(enemy, obj.transform);
+        }
+    }
+}
