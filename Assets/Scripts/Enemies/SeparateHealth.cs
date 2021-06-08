463c738f2c699d5bd2ed22ffc5f041fdcff39a5c
Luke Jones <nnblc565@gmail.com>
2021-06-08 13:47:00 +0930

    Created a system for weakpoints and enemies with multiple weak points.

---
+++ Assets/Scripts/Enemies/SeparateHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Redirects damage taken to a different health script
+/// </summary>
+public class SeparateHealth : Weakpoint
+{
+    /// <summary>
+    /// The target to redirect damage to
+    /// </summary>
+    [Tooltip("Redirects any damage taken by this health to a target")]
+    public Health m_damageDestination = null;
+    /// <summary>
+    /// Should damage be dealt also to this
+    /// </summary>
+    public bool m_alsoDealDamageToThis = false;
+    /// <summary>
+    /// Deals damage to the target and also this if m_alsoDealDamageToThis is true
+    /// </summary>
+    /// <param name="damage">The damage to deal</param>
+    public override void DoDamage(float damage)
+    {   //Check if we should deal damage to this
+        if (m_alsoDealDamageToThis)
+            //Call the base
+            base.DoDamage(damage);
+        //Modify the damage
+        damage *= m_weakpointModifier;
+        //Redirect the damage to the target
+        m_damageDestination.DoDamage(damage);
+    }
+}
---
+++ Assets/Scripts/Enemies/Weakpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Health with a damage modifier
+/// </summary>
+public class Weakpoint : Health
+{
+    /// <summary>
+    /// The damage modifier
+    /// </summary>
+    [Tooltip("The damage modifier for damaging this Health")]
+    public float m_weakpointModifier = 1;
+    /// <summary>
+    /// Multiplies the damage by m_weakpointModifier before dealing damage
+    /// </summary>
+    /// <param name="damage">The damage to deal</param>
+    public override void DoDamage(float damage)
+    {   //Modify the damage
+        damage *= m_weakpointModifier;
+        //Deal the damage
+        base.DoDamage(damage);
+    }
+}
