7e9c803288daf791b2349764a1ecd2f57811edba
Luke Jones <nnblc565@gmail.com>
2021-06-10 11:24:27 +0930

    Change the skybox on start

---
+++ Assets/Scripts/SetSkybox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Sets the skybox on start
+/// </summary>
+public class SetSkybox : MonoBehaviour
+{
+    /// <summary>
+    /// The material for the skybox
+    /// </summary>
+    public Material skybox;
+    /// <summary>
+    /// Sets the skybox
+    /// </summary>
+    void Start()
+    {
+        RenderSettings.skybox = skybox;
+    }
+    //When in the editor, keeps setting the skybox
+#if UNITY_EDITOR
+    /// <summary>
+    /// Continually sets the skybox for debugging purposes
+    /// </summary>
+    void Update()
+    {
+        RenderSettings.skybox = skybox;
+    }
+#endif
+}
