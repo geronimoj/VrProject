5ed8bd9f2e64480880821808e09657cb7566d199
Luke Jones <nnblc565@gmail.com>
2021-06-10 12:02:23 +0930

    Centered the controller infront of the player

---
+++ Assets/Scripts/CenterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterController : MonoBehaviour
+{
+    [Tooltip("How far away the RightHandAnchor is from its relative 0,0,0")]
+    public float m_controllerLength = 0.5f;
+
+    public Transform m_targetPosition = null;
+
+    public Vector3 m_offset;
+
+    private void LateUpdate()
+    {
+        if (!m_targetPosition)
+            return;
+
+        transform.position = m_offset +  m_targetPosition.position + transform.forward * m_controllerLength;
+    }
+}
