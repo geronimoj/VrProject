f8be6c97c1b62123a219f0677b40c7f9a49722e1
Luke Jones <nnblc565@gmail.com>
2021-06-16 11:28:41 +0930

    Added a safety mode to the weapon system so we can disable guns.

---
+++ Assets/Scripts/Managers/SafetyModeToggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetyModeToggle : MonoBehaviour
+{
+    public bool safetyModeValue = true;
+
+    private void Start()
+    {
+        WeaponSystem.safetyMode = safetyModeValue;
+    }
+}
