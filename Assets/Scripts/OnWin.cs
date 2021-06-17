79a39148d24788164f561030ce47db1298d37caf
Luke Jones <nnblc565@gmail.com>
2021-06-17 16:12:55 +0930

    Added some UI to tell the player they just onlocked a thing

---
+++ Assets/Scripts/OnWin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnWin : MonoBehaviour
+{
+    public UnityEvent onWin;
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+        GameManager.s_instance.OnWin.AddListener(Win);
+    }
+
+    private void Win()
+    {
+        onWin.Invoke();
+    }
+}
