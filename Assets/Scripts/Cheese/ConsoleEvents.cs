f5e2d9c879fb3bcb7a907fac3a882582e13797b3
Zaydan Bickley <pokemaniac88@gmail.com>
2021-06-17 16:22:29 +0930

    Beginning the Console Button system

---
+++ Assets/Scripts/Cheese/ConsoleEvents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleEvents : MonoBehaviour
+{
+    public List<int> currentNumbers;
+    public float maxTimer;
+    private float currentTimer;
+    public int[] sans;
+    public GameObject sansPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTimer > 0)
+            currentTimer -= Time.deltaTime;
+        else if (currentTimer < 0)
+            currentTimer = 0;
+    }
+
+    public void AddNumber(int n)
+    {
+        currentTimer = maxTimer;
+        currentNumbers.Add(n);
+    }
+}
